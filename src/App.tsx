ba4d348bdc8a781cad6f6c8d3b36df6b9ce302cb
Jonathan Macedo <jonathan_maccas@outlook.com>
2023-10-12 12:34:36 -0300

    Colocando os componentes criados e alterando o css da aplicação para deixar somente o que for de uso do arquivo principal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,18 @@
 import "./App.css";
+import AcessarConta from "./components/acesso/AcessarConta";
+import CadastrarConta from "./components/cadastro/CadastrarConta";
 
 function App() {
   return (
     <div className="container">
       <main>
-        <section id="acessar_conta">
-          <h1>Acessar conta</h1>
-          <input type="email" name="email" id="email" placeholder="E-mail" />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Senha"
-          />
-          <div id="adicionais_login">
-            <div>
-              <input type="checkbox" name="remember" id="remember" />
-              <label htmlFor="remember">Lembrar-me</label>
-            </div>
-            <div>
-              <a href="#">Esqueceu a senha?</a>
-            </div>
-          </div>
-          <button type="submit">Acessar</button>
-        </section>
+        <AcessarConta/>
         <section id="divisor">
           <div />
           <span>Ou</span>
           <div />
         </section>
-        <section id="criar_conta">
-          <h1>Cadastrar conta</h1>
-          <input type="text" name="name" id="name" placeholder="Nome" />
-          <input type="email" name="email" id="email" placeholder="E-mail" />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Senha"
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            placeholder="Confirmar Senha"
-          />
-          <button type="submit">Cadastrar</button>
-        </section>
+        <CadastrarConta/>
       </main>
     </div>
   );
